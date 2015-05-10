d4d17ed7109009b27afd2e5cb9e2b3a435e70867
Yuri Gritsai <yugritsai@gmail.com>
2015-05-10 23:30:47 +0300

    var's declaration

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,7 +13,70 @@ role = "Web Developer";
 // console.log(awesomeThoughts);
 // console.log(funThoughts);
 
+var skills = ["Python", "JavaScript", "TwitterBootstrap", "PostgreSQL", "OOP", "HTML5", "CSS3", "LESS"];
+
+var bio = {
+    "name": "Yuri Gritsai",
+    "role": "Web Developer",
+    "age": 29,
+    "skills": skills,
+    "image_url": "images/avamy.JPG",
+    "short_message": "I glad to see you here friend",
+    "contacts": {
+        "mobile": "+380500152281",
+        "email": "yugritsai@gmail.com"
+    }
+};
+
+var work = [
+    {
+        "employee": "Mr. Snack",
+        "title": "manager",
+        "location": "Kiev",
+        "dates": "2004-2007"
+    },
+    {
+        "employee": "Elcom",
+        "title": "service engineer",
+        "location": "Kiev",
+        "dates": "2008-2011"
+    }
+];
+var projects = [
+    {
+        "title": "",
+        "dates": "",
+        "description": "",
+        "image": ""
+    },
+    {
+        "title": "",
+        "dates": "",
+        "description": "",
+        "image": ""
+    }
+];
+var education = [
+    {
+        "name": "National Aviation University",
+        "location": "Kiev",
+        "degree": "electronics engineer",
+        "majors": ["BA"],
+        "url": "http://nau.edu.ua/"
+    },
+    {
+        "name": "Intro to computer science",
+        "location": "Udacity",
+        "degree": "Python developer",
+        "majors": [""],
+        "url": "http://udacity.com/"
+    }
+];
+
+
 formattedName = HTMLheaderName.replace("%data%", name);
 formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+$("#main").prepend(bio["city"]);
